861f3e25120fd27e8a7f70ef558676597cc826c4
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-12-13 23:59:05 +0100

    puppet manifest to set up nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# setup nginx with a puppet manifest
+# Excerpt from Pericles Adjovi
+
+package {
+    'nginx':
+    ensure => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+}
+
+service {'nginx':
+    ensure => running,
+}
