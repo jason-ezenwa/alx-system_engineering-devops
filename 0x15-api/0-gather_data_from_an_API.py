e8cd8d74fd3fcd6d9b373afe85edd2245b7915aa
JasonFlair <emelieobumse100@gmail.com>
2023-01-17 20:00:03 +0100

    guy

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -34,7 +34,8 @@ def fetching():
         if task_info.get('completed') is True:
             completed_tasks.append(task_info)
     completed_tasks_num = len(completed_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks_num, total_task_num))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, completed_tasks_num, total_task_num))
     # print the title of the completed tasks
     for each_task in completed_tasks:
         print("\t {}".format(each_task['title']))
