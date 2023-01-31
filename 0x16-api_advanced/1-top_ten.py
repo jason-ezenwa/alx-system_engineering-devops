ccb9d79b9034bde898f93be3ddc9b1ad4e2231be
JasonFlair <emelieobumse100@gmail.com>
2023-01-31 18:40:47 +0100

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,13 @@ import requests
 
 def top_ten(subreddit):
     headers = {"User-Agent": "CustomUser"}
-    params = {"limit": 10}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
     jsonresp = response.json()
     posts = jsonresp['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    for post in range(0, 10):
+        print(posts[post]['data']['title'])
 
