d1416a67e2d823bd6754f7fd362c593dbcd7f8da
JasonFlair <emelieobumse100@gmail.com>
2023-01-31 18:30:29 +0100

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,6 +9,7 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         print("None")
+        return
     jsonresp = response.json()
     posts = jsonresp['data']['children']
     for post in range(0, 10):
