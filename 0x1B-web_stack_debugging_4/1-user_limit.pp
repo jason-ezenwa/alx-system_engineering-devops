beeddeed0a5b8fe579cb6b903a4b3c8af2291aa8
Jason Flair <emelieobumse100@gmail.com>
2023-02-08 22:57:33 +0100

    commented user limit fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,10 @@
+# puppet manifest to make changes to the holberton user limits configuration
+
 exec {'hard nofile change':
   command  => 'sed -i "s/nofile 5/nofile 6000/" /etc/security/limits.conf',
   provider => 'shell',
   before   => Exec['soft nofile change'],
- }
+}
 exec {'soft nofile change':
   command  => 'sed -i "s/nofile 4/nofile 5000/" /etc/security/limits.conf',
   provider => 'shell',
