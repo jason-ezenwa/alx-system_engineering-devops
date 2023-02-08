0e350ed79074eb174d14fb25d3c825845b2cd69f
Jason Flair <emelieobumse100@gmail.com>
2023-02-08 22:51:10 +0100

    user limit fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 exec {'hard nofile change':
   command  => 'sed -i "s/nofile 5/nofile 6000/" /etc/security/limits.conf',
   provider => 'shell',
-  before   => 'soft nofile change',
+  before   => Exec['soft nofile change'],
  }
 exec {'soft nofile change':
   command  => 'sed -i "s/nofile 4/nofile 5000/" /etc/security/limits.conf',
