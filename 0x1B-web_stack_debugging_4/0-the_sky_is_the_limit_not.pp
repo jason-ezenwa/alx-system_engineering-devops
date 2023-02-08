048db48bdc286a01e8fbea8c05bf09b9db61953f
Jason Flair <emelieobumse100@gmail.com>
2023-02-08 16:20:49 +0100

    first

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # a puppet manifest that increases the open files limit
 
 exec {'sed':
-  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"" /etc/default/nginx',
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"/" /etc/default/nginx',
   provider => 'shell',
 }
 
